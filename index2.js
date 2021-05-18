3abfd6d632ae981c8d3e68c7ffa2f1ae582e8cc3
Your Name <you@example.com>
2021-05-18 13:44:17 +0200

    -

--- index2.js
+++ index2.js
@@ -6,17 +6,22 @@ const writeFile=promisify(fs.writeFile);
 
 
     const convertCSVtoJSON=async(csvFilePath,jsonPath)=>{
-        console.log(csvFilePath)
+        try{
         const jsonArr= await csv().fromFile(csvFilePath);
         const jsonString=JSON.stringify(jsonArr);
         await writeFile(jsonPath,jsonString);
         console.log(`JSON file saved at:${jsonPath}`)
 
+        }catch(err){
+            console.log("Something went wrong, Could not write json to: hello.json")
+        }
+       
+        
+
     }
     if(args.length===1){
         convertCSVtoJSON(args[0],"demo.json");
     }else if(args.length===2){
-
          jsonPath=args[1];
        convertCSVtoJSON(args[0],jsonPath);
     
